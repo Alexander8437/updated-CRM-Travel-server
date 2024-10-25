5b24f8e066df2f5815d8cb79e0c6006a475992e8
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2024-10-25 13:47:43 +0530

    adding cross origin  header in room master and room type

--- src/main/java/com/MotherSon/CRM/controller/RoomTypesController.java
+++ src/main/java/com/MotherSon/CRM/controller/RoomTypesController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
